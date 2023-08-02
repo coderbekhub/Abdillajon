73eb57975b6d700074babd91f686ef04eac2b6d2
coderbekhub <coderbek@mail.ru>
2023-08-02 18:36:20 +0500

    ...

--- abdilla_portfolio/src/Components/Home.jsx
+++ abdilla_portfolio/src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import About from './About'
 
 export default function Home() {
   return (
     <>
     <Header />
+    <About />
     </>
   )
 }
