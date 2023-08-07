39ba57b0ef6c1254856dfc26be79bfe1957a36c9
coderbekhub <coderbek@mail.ru>
2023-08-07 12:46:49 +0500

    ...

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -11,6 +11,7 @@ export default function Home() {
     <About />
     <Partners />
     <Project />
+    
     </>
   )
 }
--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import Activity from '../img/Activity.svg'
+import Chart from '../img/Chart.svg'
+import Category from '../img/Category.svg'
+import EditSquare from '../img/EditSquare.svg'
 
 export default function Service() {
   return (
-    <div>Service</div>
+    <section className='serviceSection'>
+      <div className="container">
+        <h2>02 <span>my service</span></h2>
+        <div className='serviceContent'>
+          <div className='serviceCard'>
+            <img src={Activity} alt="..." />
+            <h2>UI/UX Design</h2>
+            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Vuis enim velit ullamco</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
