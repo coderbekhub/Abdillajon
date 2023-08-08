9166afb099bfd4fff1b30efe82d2b1b86ce4c3a0
coderbekhub <coderbek@mail.ru>
2023-08-08 12:18:21 +0500

    ...

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,6 +4,8 @@ import NavFcBook from '../img/NavFsBook.svg'
 import NavLN from '../img/NavLN.svg'
 import NavSite from '../img/NavSite.svg'
 import Call from '../img/Call.svg'
+import MessageContact from '../img/MessageContact.svg'
+import loationContact from '../img/locationContact.svg'
 
 export default function Contact() {
   return (
@@ -13,20 +15,20 @@ export default function Contact() {
           <div className='contactInfo'>
             <h2> 05 <span>contact</span></h2>
             <div>
-              {/* <img src={Call} alt="..." /> */}
-              <div>
-                <h3>Phone</h3>
-                <a href="tel:+998977770777">(219) 555-0114</a>
-              </div>
+              <img src={Call} alt="..." />
+              <h3>Phone:</h3>
+              <a href="tel:+998977770777">(219) 555-0114</a>
             </div>
 
             <div>
-              <h3>Email</h3>
+              <img src={MessageContact} alt="..." />
+              <h3>Email:</h3>
               <a href="tel:+998977770777">robertfox@example.com</a>
             </div>
 
             <div>
-              <h3>Address</h3>
+              <img src={loationContact} alt="..." />
+              <h3>Address:</h3>
               <span>4517 Washington Ave. Manchester, Kentucky 39495</span>
             </div>
 
@@ -36,7 +38,8 @@ export default function Contact() {
                   <div>
                     <img src={NavInsta} alt="..." />
                   </div>
-                </a></li>
+                </a>
+              </li>
               <li>
                 <a href="#">
                   <div>
@@ -63,14 +66,11 @@ export default function Contact() {
             <h2>I’m always open to discussing <span>product design work</span> or partnership </h2>
 
             <div className='col-md-6 col-12'>
-              {/* <label htmlFor="name">Your name*</label> <br /> */}
               <input type="text" id='name' required placeholder='Enter your name here' /><br />
 
-              {/* <label htmlFor="address">Email Address*</label> <br /> */}
               <input type="number" id='number' required placeholder='Enter your phone number' /> 
             </div>
             <div className='col-md-12 col-12'>
-              {/* <label htmlFor="message">Message</label> <br /> */}
               <textarea name="message" id="message" cols="30" rows="10" placeholder='Write your message here'></textarea>
             </div>
               <button>send message</button>
