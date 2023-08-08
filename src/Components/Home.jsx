3495735f2146a4577d2c06273317ae038308138f
coderbekhub <coderbek@mail.ru>
2023-08-08 16:59:39 +0500

    ...

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import Header from './Header'
 import About from './About'
-import Partners from './Partners'
-import Project from './Project'
-import Service from './Service'
-import Portfolio from './Portfolio'
-import Testimonial from './Testimonial'
-import Contact from './Contact'
+// import Partners from './Partners'
+// import Project from './Project'
+// import Service from './Service'
+// import Portfolio from './Portfolio'
+// import Testimonial from './Testimonial'
+// import Contact from './Contact'
 
 export default function Home() {
   return (
     <>
     <Header />
     <About />
-    <Partners />
+    {/* <Partners />
     <Project />
     <Service />
     <Portfolio />
     <Testimonial />
-    <Contact />
+    <Contact /> */}
     </>
   )
 }
